5aa8617f4aa7b90aee2ac130e5e3a28365bc191c
Ignacio <ignacampos@hotmail.com>
2025-02-28 04:03:48 -0300

    arreglos por 0i23'213012q3 vez

--- zentrip/src/components/ToursSection.jsx
+++ zentrip/src/components/ToursSection.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { getDestinationProducts } from './viatorClient.ts';
+import { getDestinationProducts } from './viatorClient';
 
 const ToursSection = () => {
   const [tours, setTours] = useState([]);
