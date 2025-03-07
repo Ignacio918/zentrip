1b85cf6bf95c8532d6176ddf0265771cd5d5d90e
Ignacio <ignacampos@hotmail.com>
2025-03-06 21:36:22 -0300

    arreglos

--- zentrip/src/pages/Landing.jsx
+++ zentrip/src/pages/Landing.jsx
@@ -1,18 +1,16 @@
 import React, { Suspense, lazy } from 'react';
-import Navbar from '../components/Navbar';
 import { motion } from 'framer-motion';
 import '../styles/LandingPage.css';
-import ToursSection from '../components/ToursSection'; // Importamos el componente
-
-const Chat = lazy(() => import('../components/Chat')); // Carga diferida
-import generateItinerary from '../cohereClient'; // Importamos la función
+import ToursSection from '../components/ToursSection';
+import generateItinerary from '../cohereClient';
 import HotelsSection from '../components/HotelsSection';
 import RestaurantsSection from '../components/RestaurantsSection';
+
+const Chat = lazy(() => import('../components/Chat'));
 
 const Landing = () => {
   return (
     <section className="hero-section">
-      <Navbar />
       <div className="hero-content container mx-auto px-8">
         <motion.h1
           initial={{ opacity: 0, y: 20 }}
@@ -25,22 +23,22 @@ const Landing = () => {
           de planificar
           <br /> tu próxima aventura
         </motion.h1>
+
         <Suspense fallback={<div>Loading Chat...</div>}>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2, ease: 'easeOut' }}
-            className="chat-container mt-12"
+            className="chat-container"
           >
             <Chat onSubmit={generateItinerary} />
           </motion.div>
         </Suspense>
-        <div className="mt-16">
-          {' '}
-          {/* Espaciado prudencial de 16 unidades */}
+
+        <div className="w-full space-y-16 mt-16">
           <ToursSection />
           <HotelsSection />
-          <RestaurantsSection />          
+          <RestaurantsSection />
         </div>
       </div>
     </section>
